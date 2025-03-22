36064a4ba7ed33ab60a058ab8355cc08f7db285f
pcnslt <info@pcnslt.com>
2025-03-21 20:15:05 -0400

    Add graphQl code. Add pathlib example.  Add basic streamlit library.

--- facebook_graph_ql.py
+++ facebook_graph_ql.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
-import os, requests
+import os, requests, json
 
 load_dotenv('.secrets')
 
-facebook_utility_app_key = os.environ('FACEBOOK_UTILITY_APP_KEY')
-url = f"https://graph.facebook.com/v22.0/me?fields=id%2Cname&access_token={facebook_utility_app_key}"
-
+facebook_utility_app_key = os.getenv('FACEBOOK_UTILITY_APP_KEY')
+url = f"https://graph.facebook.com/v22.0/10153886934964706/picture?access_token={facebook_utility_app_key}"
 response = requests.get(url, timeout=120)
-print(response.text)
+
+data = json.loads(response.text)
+print(data)
